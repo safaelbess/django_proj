f5e7416a523bc60ef8bc67cba66e06eaf0399631
safaelbess <136888825+safaelbess@users.noreply.github.com>
2024-07-27 13:48:41 +0300

    django
    djang

--- venv/urls.py
+++ venv/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('login/',login_page,name='login'),
     path('register/', register, name='register' ),
     path('logout/', logout_page, name='logout'),
+    ##path('teacher/', teacher_page , name='teacher' ),
+    ##path('student/', student_page , name='student'),
+    ##path('subject/', subject_page , name='subject'),
+    ##path('about/', about_page ,name='about'),
 
 
 ]
